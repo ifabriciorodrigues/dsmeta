d19b31d552416c890a57c1762ec06441c0b48453
Fabrício Rodrigues <earthbornshepard@hotmail.com>
2022-07-12 19:39:42 -0300

    feat: functional DatePicker

---
+++ frontend/src/components/SalesCard/SalesCardHeader/SalesCardHeader.constants.ts
@@ -0,0 +1,1 @@
+export const ONE_YEAR_AGO_DATE = new Date(new Date().setDate(new Date().getDate() - 365));
--- frontend/src/components/SalesCard/SalesCardHeader/index.tsx
+++ frontend/src/components/SalesCard/SalesCardHeader/index.tsx
@@ -1,32 +1,39 @@
 import { DatepickerWrapper, FormContainer, Heading } from './styles'
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
+import { useState } from 'react';
+import { ONE_YEAR_AGO_DATE } from './SalesCardHeader.constants';
 
-const SalesCardHeader = () => (
-  <div>
-    <Heading>Vendas</Heading>
+const SalesCardHeader = () => {
+  const [startDate, setStartDate] = useState<Date>(ONE_YEAR_AGO_DATE);
+  const [endDate, setEndDate] = useState<Date>(new Date());
 
-    <DatepickerWrapper />
+  return (
     <div>
-      <FormContainer>
-        <DatePicker
-          selected={new Date()}
-          onChange={(date: Date) => {}}
-          dateFormat="dd/MM/yyyy"
-          className="datepicker"
-        />
-      </FormContainer>
-      <FormContainer>
-        <DatePicker
-          selected={new Date()}
-          onChange={(date: Date) => {}}
-          dateFormat="dd/MM/yyyy"
-          className="datepicker"
-        />
-      </FormContainer>
-    </div>
+      <Heading>Vendas</Heading>
 
-  </div>
-)
+      <DatepickerWrapper />
+      <div>
+        <FormContainer>
+          <DatePicker
+            selected={startDate}
+            onChange={(d: Date) => setStartDate(d)}
+            dateFormat="dd/MM/yyyy"
+            className="datepicker"
+          />
+        </FormContainer>
+        <FormContainer>
+          <DatePicker
+            selected={endDate}
+            onChange={(d: Date) => setEndDate(d)}
+            dateFormat="dd/MM/yyyy"
+            className="datepicker"
+          />
+        </FormContainer>
+      </div>
+
+    </div>
+  )
+}
 
 export default SalesCardHeader
