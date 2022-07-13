a7a947ea6abfe51f94f1a65012c8e90f4f4784e2
Fabrício Rodrigues <earthbornshepard@hotmail.com>
2022-07-13 19:52:18 -0300

    feat: date range for get all sales endpoint

--- backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
@@ -3,9 +3,13 @@ package com.devsuperior.dsmeta.service;
 import com.devsuperior.dsmeta.domain.Sale;
 import com.devsuperior.dsmeta.repository.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
@@ -13,7 +17,12 @@ public class SaleService {
     @Autowired
     private SaleRepository saleRepository;
 
-    public List<Sale> getAll() {
-        return saleRepository.findAll();
+    public Page<Sale> getAll(String minDate, String maxDate,Pageable pageable) {
+        LocalDate TODAY_DATE = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals("") ? TODAY_DATE.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? TODAY_DATE : LocalDate.parse(maxDate);
+
+        return saleRepository.findSales(min, max, pageable);
     }
 }
