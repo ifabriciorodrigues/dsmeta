d63aed1960b64ff15d7b2b2d5315b305afc4b091
Fabrício Rodrigues <earthbornshepard@hotmail.com>
2022-07-13 19:34:53 -0300

    feat: get all sales endpoint

---
+++ backend/src/main/java/com/devsuperior/dsmeta/repository/SaleRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dsmeta.repository;
+
+import com.devsuperior.dsmeta.domain.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
