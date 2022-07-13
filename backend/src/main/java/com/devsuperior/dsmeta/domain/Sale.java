ae1a0e813eb6ee9a960a8f1d0877f476e0b06d3b
Fabrício Rodrigues <earthbornshepard@hotmail.com>
2022-07-13 19:25:43 -0300

    add: Sale domain and H2 table

---
+++ backend/src/main/java/com/devsuperior/dsmeta/domain/Sale.java
@@ -0,0 +1,68 @@
+package com.devsuperior.dsmeta.domain;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private Integer timesVisited;
+    private Integer quantity;
+    private Double total;
+    private LocalDate date;
+
+    public Sale() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public Integer getTimesVisited() {
+        return timesVisited;
+    }
+
+    public void setTimesVisited(Integer timesVisited) {
+        this.timesVisited = timesVisited;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
